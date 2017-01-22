9afe1452c46e410e700f487d513107f6c0c26574
Marvin J. Cortés <marvin.cortes@hotmail.com>
2017-01-22 16:40:10 -0600

    Update addCourse.php file

--- php/addProject.php
+++ php/addProject.php
@@ -1,10 +1,10 @@
 <?php 
   include("dbConnect.php");
 
-  if(isset($_GET['courseId']) && isset($_GET['name']) && isset($_GET['projectTypeId']) isset($_GET['methodologyId'])) {
+  if(isset($_GET['courseId']) && isset($_GET['name']) && isset($_GET['projectTypeId']) && isset($_GET['methodologyId'])) {
 
 
-    $query = "INSERT INTO project(name, summary, technology, methodologyId, projectTypeId, courseId) VALUES ('".$_GET['name']."','".$_GET['summary']. "','".$_GET['technology']."',".$_GET['methodologyId'].",".$_GET['projectTypeId'].",".$_GET['courseId']."')"; 
+    $query = "INSERT INTO project (name, summary, technology, methodologyId, projectTypeId, courseId) VALUES ('".$_GET['name']."','".$_GET['summary']. "','".$_GET['technology']."',".$_GET['methodologyId'].",".$_GET['projectTypeId'].",".$_GET['courseId']."')"; 
     $result = $conn->query($query);
 
     echo $result->insert_id;
